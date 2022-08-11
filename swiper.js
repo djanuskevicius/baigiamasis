e8be3f00b1fe5886e5387a9436f91da72eacaa9a
djanuskevicius <106997462+djanuskevicius@users.noreply.github.com>
2022-08-11 18:49:53 +0300

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  speed: 1000,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    1000: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      allowSlidePrev: true,
+      allowSlideNext: true,
+    }
+  }
+});
